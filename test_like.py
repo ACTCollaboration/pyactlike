3a04f5e8efd8acd86686c408aa07bc9add909787
xzackli <xzackli@gmail.com>
2020-04-10 16:48:07 -0400

    paste old likelihood in there

---
+++ test_like.py
@@ -0,0 +1,14 @@
+def test():
+    """
+    This function tests out the basic functionality of this likelihood code.
+    """
+    pass
+    # filename = self.data_dir + "planck2015.dat"
+
+    # tt_lmax = 6000
+    # dum1, cell_tt, cell_te, cell_ee, dum2, dum3 = \
+    #     np.genfromtxt(filename, delimiter=None, unpack=True, max_rows=tt_lmax-1)
+    # like = self.loglike(cell_tt, cell_te, cell_ee, 1.0)
+
+    # print("Expected: 147.747797921459")
+    # print("Found   : " + '{0:.12f}'.format(2*like))
